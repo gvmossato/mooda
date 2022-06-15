5898e554ec2bcfe30830d84b3bde183448fb79e8
gvmossato_loyall <gabriel@loyall.com.br>
2022-06-15 00:18:52 -0300

    basic server routing

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
-const express = require("express");
+const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
+
+app.get("*", (req, res) => {
+    res.json({ message: "404" });
+  });
