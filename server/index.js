caff69d147e6ee59cad9c35a7e362c7ba54593c3
gvmossato_loyall <gabriel@loyall.com.br>
2022-06-15 22:36:31 -0300

    add basic development server

--- server/index.js
+++ server/index.js
@@ -3,13 +3,26 @@ const express = require('express');
 const router = require('./routes');
 
 
+
+(async () => {
+    const sequelize = require('./db');
+    const Sensors = require('./models');
+
+    try {
+        const resultado = await sequelize.sync();
+        console.log(resultado);
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
-});
+})
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
---
+++ server/models.js
@@ -0,0 +1,46 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('./db');
+
+const Sensors = sequelize.define('sensors', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    temperature: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    luminosity: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    soilHumidity: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    airHumidity: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    airQuality: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    happiness: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    presence: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    },
+})
+
+module.exports = Sensors;
