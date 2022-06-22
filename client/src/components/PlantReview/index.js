0a83bff537a74dca479e35ce19fdd77f15ee82f5
gvmossato_loyall <gabriel@loyall.com.br>
2022-06-22 17:04:29 -0300

    add basic global styles and plant-review styles

--- client/src/components/PlantReview/index.js
+++ client/src/components/PlantReview/index.js
@@ -1,5 +1,7 @@
 import { BsCheckCircleFill, BsXCircleFill } from 'react-icons/bs';
 import getStatus from '../../utils/getStatus';
+import moodaLogo from '../../assets/mooda.png'
+import plant from '../../assets/plant.png'
 
 import HappinessBar from '../HappinessBar';
 
@@ -32,22 +34,30 @@ function PlantReview() {
 
     return (
         <aside>
-            <img src="" alt="mooda-logo" />
-            <div>
-                <img src="" alt="plant-pic"/>
+
+                <img src={moodaLogo} alt="mooda-logo" />
+
+            <div className="plant-review">
+                <div className="plant-frame">
+                    <img src={plant} alt="plant"/>
+                </div>
 
                 { <HappinessBar /> }
 
-                {
-                    status.map(el => { return (
-                        <div id={el.id} key={el.id}>
-                            <p>{el.name}</p>
-                            <p className={"icon " + (el.isFine ? "check" : "cross")}>
-                                { el.isFine ? <BsCheckCircleFill /> : <BsXCircleFill /> }
-                            </p>
-                        </div>
-                    )})
-                }
+                <div className="status-collection">
+                    {
+                        status.map(el => {
+                            return (
+                                <div className={"status " + el.id} key={el.id}>
+                                    <p>{el.name}</p>
+                                    <p className={"icon " + (el.isFine ? "check" : "cross")}>
+                                        { el.isFine ? <BsCheckCircleFill /> : <BsXCircleFill /> }
+                                    </p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </aside>
     );
